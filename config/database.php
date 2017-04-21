51b2c4c8c56dcd73f12ecfe58f8bd06d05e9714a
Melvin Daytree <melvin.daytree@uconn.edu>
2017-04-21 15:00:08 -0400

    added vagrant support and created a relational database link

--- config/database.php
+++ config/database.php
@@ -56,7 +56,7 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'calc'),
+            'database' => env('DB_DATABASE', 'calcd'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
